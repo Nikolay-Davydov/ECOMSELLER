210b3c10386ff17a0cc3bcb3c21f888c3053d258
Nikolay-Davydov <davidovns70@gmail.com>
2022-06-27 16:06:03 +0300

    add def get_candidates_with_offset, correction of a comment on error 429

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def load_config():
     key = conf.get('default', 'KEY')
     url = conf.get('default', 'URL')
     version = conf.get('default', 'VERSION')
-    return {'id': id, 'key': key, 'url': url, 'version': version}
+    repeat = conf.get('default', 'RETRY_COUNT')
+    return {'id': id, 'key': key, 'url': url, 'version': version, 'repeat': repeat}
 
 
 config = load_config()
@@ -17,7 +18,7 @@ seller = ozone_seller.OzoneSeller(config)
 all_actions = seller.get_actions()
 products_for_actions = {}
 for action in all_actions:
-    products_for_action = seller.get_candidates(action['id'])
+    products_for_action = seller.get_candidates(action['id'], 10)
     products_for_actions[action['id']] = {'products': products_for_action, 'info': action}
 
 print(products_for_actions)
--- ozone_seller.py
+++ ozone_seller.py
@@ -1,3 +1,5 @@
+import math
+import time
 import requests
 
 
@@ -8,6 +10,7 @@ class OzoneSeller:
         self.key = data['key']
         self.url = data['url']
         self.version = data['version']
+        self.repeat = data['repeat']
 
     def get_headers(self):
         return {
@@ -20,16 +23,28 @@ class OzoneSeller:
         all_actions = self.do_request('get', '/actions')
         return all_actions
 
-    def get_candidates(self, action_id):
+    def get_candidates(self, action_id, limit):
         '''Получение товаров подходящих к акции'''
+        result = self.get_candidates_with_offset(action_id, limit, 0)
+        total = getattr(result, 'total', 0)
+        candidates = getattr(result, 'products', {})
+        if total > limit:
+            quantity = math.ceil(total / limit)
+            page = 1
+            while page <= quantity:
+                result = self.get_candidates_with_offset(action_id, limit, page * limit + 1)
+                candidates.append(getattr(result, 'products', {}))
+                page += 1
+        return candidates
+
+    def get_candidates_with_offset(self, action_id, limit, offset):
         params = {
             'action_id': action_id,
-            'limit': 10,
-            'offset': 0,
+            'limit': limit,
+            'offset': offset,
         }
         result = self.do_request('post', '/actions/candidates', params)
-        candidates = getattr(result, 'products', {})
-        return candidates
+        return result
 
     def add_product_to_action(self, action_id, product_id, action_price, stock):
         '''Добавление продукта к акции'''
@@ -47,7 +62,7 @@ class OzoneSeller:
         products_actions = getattr(result, 'product_ids', {})
         return products_actions
 
-    def do_request(self, type_request, type, params=None):
+    def do_request(self, type_request, type, params=None, repetition=0):
         '''Выполнение запросов'''
         url = self.url + self.version + type
         headers = self.get_headers()
@@ -55,6 +70,11 @@ class OzoneSeller:
             result = requests.get(url, headers=headers)
         else:
             result = requests.post(url, headers=headers, params=params)
+        # Рекурсия не самое оптимально решение, применено для минимальной корректировки остального кода
+        while result.status_code == 429 and repetition < self.repeat:
+            time.sleep(1)
+            repetition += 1
+            result = self.do_request(type_request, type, params, repetition)
         if result.status_code == 200:
             return result.json()['result']
         else:
